d1dbc69cbb8f6ad76a046deee71349e547f29c9a
赵如意 <3438605753@qq.com>
2025-09-09 17:51:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# 在这个文件下编写代码，题目具体要求见README.md文件
+# weight = input()
+
+if weight[-2:] == 'kg':
+    kg = eval(weight[0:-2])
+    pd = kg * 2.2046
+    print("对应的英制重量为{:.3f}磅".format(pd))
+elif weight[-2:] == 'pd':
+    pd = eval(weight[0:-2])
+    kg = pd / 2.2046
+    print("对应的公制重量为{:.3f}公斤".format(kg))
+else:
+    print("输入格式错误!")

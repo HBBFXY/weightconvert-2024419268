c6c7212c26eca3aaacefd9b363dfa02de17c2fa1
赵如意 <3438605753@qq.com>
2025-09-10 17:30:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 def weight_converter(weight,unit):
     if unit=='kg':
-        #将千克转换为磅
         converted_weight=weight*2.2046
         return f"{converted_weght:.3f}pd"
     elif unit=='pd':
-        #将磅转换为千克
         converted_weight=weight/2.2046
         return f"{converted_weight:.3f}kg"
     else:
